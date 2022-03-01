774851b61358fea86b1dbd959c86d5d5249972f8
eniskastrati <eniskastrati18@gmail.com>
2022-03-01 12:02:11 +0100

    Func: to generate a choice for computer

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 
-let userChoice;
+let userChoice,computerChoice;
 //
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',(e) => {
     userChoice = e.target.id;
@@ -14,6 +14,13 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 
 let generateComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * possibleChoices.length + 1);//or just 3 as we have only 3 choises
-    console.log(randomNumber)
+    
+    if(randomNumber === 1){
+        computerChoice = 'rock';
+    }else if(randomNumber === 2){
+        computerChoice = 'paper'
+    }else{
+        computerChoice = 'scissors' 
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
 };
- 
