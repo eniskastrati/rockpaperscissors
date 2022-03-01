2a31ef44fbe36f665955043d8b149104275e6ef8
eniskastrati <eniskastrati18@gmail.com>
2022-03-01 12:13:09 +0100

    Added the function to get the results and to display it

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 
-let userChoice,computerChoice;
-//
+//varibale declaration
+let userChoice,computerChoice,result;
+
+//setting an click event Listener for every possibleChoice and displaying the value
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',(e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
 }))
 
-let generateComputerChoice = () => {
+//functions
+const generateComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * possibleChoices.length + 1);//or just 3 as we have only 3 choises
     
     if(randomNumber === 1){
@@ -23,4 +27,23 @@ let generateComputerChoice = () => {
         computerChoice = 'scissors' 
     }
     computerChoiceDisplay.innerHTML = computerChoice;
+};
+
+const getResult = () => {
+    if(computerChoice === userChoice){
+        result = "It's a draw";
+    }else if (computerChoice === 'rock' && userChoice === 'paper'){
+        result = 'You win!';
+    }else if (computerChoice === 'rock' && userChoice === 'scissors'){
+        result = 'You lose!';
+    }else if (computerChoice === 'paper' && userChoice === 'rock'){
+        result = 'You lose!';
+    }else if (computerChoice === 'paper' && userChoice === 'scissors'){
+        result = 'You win!';
+    }else if (computerChoice === 'scissors' && userChoice === 'rock'){
+        result = 'You win!';
+    }else if (computerChoice === 'scissors' && userChoice === 'paper'){
+        result = 'You lose!';
+    }
+    resultDisplay.innerHTML = result;
 };
